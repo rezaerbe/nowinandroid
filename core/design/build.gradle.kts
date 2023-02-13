bd2d6ea2f1a1e04a503ad57f4166aea2a57e78e5
rezaerbe <rezarb88@gmail.com>
2023-02-13 16:15:02 +0700

    Fix lint

--- core/design/build.gradle.kts
+++ core/design/build.gradle.kts
@@ -1,7 +1,13 @@
+@file:Suppress("UnstableApiUsage")
+
 plugins {
     id("nowinandroid.android.library.view")
 }
 
 android {
     namespace = "com.erbe.nowinandroid.core.design"
+
+    lint {
+        disable.add("UnusedResources")
+    }
 }
